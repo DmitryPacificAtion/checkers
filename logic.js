1c6a296ce7e0915ff6f7f1c4f010043a213d6213
Dmyto Bruslik <Dmytro_Bruslik@external.mckinsey.com>
2021-11-16 19:06:24 +0200

    Implement init logic

--- logic.js
+++ logic.js
@@ -4,13 +4,33 @@ const defaultFriends = [
   'a3', 'c3', 'e3', 'g3',
 ];
 
+const defaultAliens = [
+  'b8', 'd8', 'f8', 'h8',
+  'a7', 'c7', 'e7', 'g7',
+  'b6', 'd6', 'f6', 'h6',
+];
+
+const handleClick = ({ target }) => {
+  target.classList.add('animate');
+};
+
 function setCheckers() {
   const fields = document.querySelectorAll('[id]');
-  console.log('init', fields);
 
-  fields.forEach(field => {
-    // const id = field.id;
-    console.log('field', field);
+  fields.forEach((field, index) => {
+    const checker = document.createElement('div');
+    field.addEventListener('click', handleClick);
+    if (defaultFriends.includes(field.id)) {
+      checker.classList.add('friend');
+      checker.setAttribute('data-friend', index);
+      field.appendChild(checker);
+      return;
+    }
+    if (defaultAliens.includes(field.id)) {
+      checker.classList.add('alien');
+      checker.setAttribute('data-alien', index);
+      field.appendChild(checker);
+    }
   })
 };
 

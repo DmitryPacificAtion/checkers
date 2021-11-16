583b3a594be3698988ad3a6bcff1c1993980459e
Dmyto Bruslik <Dmytro_Bruslik@external.mckinsey.com>
2021-11-16 19:20:48 +0200

    Refactored init logic, upd constants

--- logic.js
+++ logic.js
@@ -1,39 +1,43 @@
-const defaultFriends = [
-  'a1', 'c1', 'e1', 'g1',
-  'b2', 'd2', 'f2', 'h2',
-  'a3', 'c3', 'e3', 'g3',
-];
+const FRIEND = 'friend';
+const ALIEN = 'alien';
 
-const defaultAliens = [
-  'b8', 'd8', 'f8', 'h8',
-  'a7', 'c7', 'e7', 'g7',
-  'b6', 'd6', 'f6', 'h6',
-];
+const defaultArrangements = {
+  [FRIEND]: [
+    'a1', 'c1', 'e1', 'g1',
+    'b2', 'd2', 'f2', 'h2',
+    'a3', 'c3', 'e3', 'g3',
+  ],
+  [ALIEN]: [
+    'b8', 'd8', 'f8', 'h8',
+    'a7', 'c7', 'e7', 'g7',
+    'b6', 'd6', 'f6', 'h6',
+  ]
+};
 
 const handleClick = ({ target }) => {
   target.classList.add('animate');
 };
 
-function setCheckers() {
+const putCheckerToTheBoard = (name, field, index) => {
+  if (defaultArrangements[name].includes(field.id)) {
+    const checker = document.createElement('div');
+    checker.addEventListener('click', handleClick);
+
+    checker.classList.add(name);
+    checker.setAttribute(`data-${name}`, index);
+    field.appendChild(checker);
+  }
+};
+
+function init() {
   const fields = document.querySelectorAll('[id]');
 
   fields.forEach((field, index) => {
-    const checker = document.createElement('div');
-    field.addEventListener('click', handleClick);
-    if (defaultFriends.includes(field.id)) {
-      checker.classList.add('friend');
-      checker.setAttribute('data-friend', index);
-      field.appendChild(checker);
-      return;
-    }
-    if (defaultAliens.includes(field.id)) {
-      checker.classList.add('alien');
-      checker.setAttribute('data-alien', index);
-      field.appendChild(checker);
-    }
+    putCheckerToTheBoard(FRIEND, field, index);
+    putCheckerToTheBoard(ALIEN, field, index);
   })
 };
 
 (function() {
-  setCheckers();
+  init();
 })()

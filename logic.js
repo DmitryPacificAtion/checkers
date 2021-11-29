296017f649999d03bb389695458fb8f389edb480
Dmyto Bruslik <Dmytro_Bruslik@external.mckinsey.com>
2021-11-29 23:09:59 +0200

    Finish logic with highlighting enemy

--- logic.js
+++ logic.js
@@ -10,15 +10,25 @@ const players = {
 
 const defaultArrangements = {
   [FRIEND]: [
-    'a1', 'c1', 'e1', 'g1',
-    'b2', 'd2', 'f2', 'h2',
-    'a3', 'c3', 'e3', 'g3',
+    'a7', 'g7', 'e1',
+    'b2', 'h2',
   ],
   [ALIEN]: [
-    'f4', 'b8', 'd8', 'f8', 'h8',
-    'a7', 'c7', 'e7', 'g7',
-    'b6', 'd6', 'f6', 'h6',
-  ]
+    'b8', 'f8', 'h8',
+    'b6', 'f6', 'h6',
+    'a3', 'c3', 'f2',
+    'a1', 'c1', 'g1'
+  ],
+  // [FRIEND]: [
+  //   'a1', 'c1', 'e1', 'g1',
+  //   'b2', 'd2', 'f2', 'h2',
+  //   'a3', 'c3', 'e3', 'g3',
+  // ],
+  // [ALIEN]: [
+  //   'f4', 'b8', 'd8', 'f8', 'h8',
+  //   'a7', 'c7', 'e7', 'g7',
+  //   'b6', 'd6', 'f6', 'h6',
+  // ]
 };
 const state = {
   fields: null,
@@ -26,21 +36,28 @@ const state = {
 };
 
 const _do = (...args) => f => f(...args);
+const getCharAt = p => p.toString().charCodeAt();
+const getCoords = id => id.split('').map(getCharAt);
+const getCharFrom = n => String.fromCharCode(n);
+const add = ([x, y]) => x + y;
+const div = ([x, y]) => {
+  if (Array.isArray(x) && Array.isArray(y)) {
+    const [ firstX, lastX ] = x;
+    const [ firstY, lastY ] = y;
+    return [div([firstX, firstY]), div([lastX, lastY])];
+  }
+  return x - y;
+};
+
+const addWith = ([x, y], n) => [add([x, n]), add([y, n])];
+const divWith = ([x, y], n) => [div([x, n]), div([y, n])];
+
 const oppositePlayer = () => players[+!players[state.turn]];
 
-const getNewChar = n => String.fromCharCode(n);
-
-const topRight = (coords, n) =>
-  coords.map(p => getNewChar(p.charCodeAt() + n)).join('');
-
-const topLeft = ([x, y], n) => 
-  [getNewChar(x.charCodeAt() + n), getNewChar(y.charCodeAt() - n)].join('');
-
-const bottomLeft = (coords, n) =>
-  coords.map(p => getNewChar(p.charCodeAt() - n)).join('');
-
-const bottomRight = ([x, y], n) =>
-  [getNewChar(x.charCodeAt() - n), getNewChar(y.charCodeAt() + n)].join('');
+const topRight = (coords, n) => addWith(coords, n).map(getCharFrom).join('');
+const topLeft = ([x, y], n) => [div([x, n]), add([y, n])].map(getCharFrom).join('');
+const bottomLeft = (coords, n) => divWith(coords, n).map(getCharFrom).join('');
+const bottomRight = ([x, y], n) => [add([x, n]), div([y, n])].map(getCharFrom).join('');
 
 const directions = {
   tr: topRight,
@@ -49,26 +66,43 @@ const directions = {
   br: bottomRight
 };
 
+const convertToDirections = ([x, y]) => {
+  if (x > 0 && y > 0) {
+    return directions.tr;
+  } else if (x < 0 && y > 0) {
+      return directions.tl;
+  } else if (x > 0 && y < 0) {
+      return directions.br;
+  } else if (x < 0 && y < 0) {
+      return directions.bl;
+  } else {
+    return;
+  }
+}
+
 Object.defineProperty(directions, 'diff', {
-  value: (a, b) => {
-    // TBD found dif
-  },
+  value: (a, b) => convertToDirections(div([getCoords(a), getCoords(b)])),
 });
 
 const posibleNearbyIds = (coords) => (n) => Object.values(directions).map(_do(coords, n));
 
-const highlightPossibleSteps = steps => steps.map(step => step.classList.add('possible-step'));
+const highlightPossibleSteps = steps => steps.map(step => step?.classList?.add('possible-step'));
 
 const calculatePossibleSteps = (id) => {
   // const res = variants.map(id => state.fields.find(i => i.id === id)).filter(j => j && !j.hasChildNodes());
-  const res = posibleNearbyIds(id.split(''))(1)
+  const res = posibleNearbyIds(getCoords(id))(1)
     .map(id => document.getElementById(id))
     .filter(i => i && !i.lastChild?.classList?.contains(state.turn))
     // .map(i => i.lastChild?.classList?.contains(state.turn));
     .map(i => {
       const hasOpposite = i.lastChild?.classList?.contains(oppositePlayer());
-      if(hasOpposite) {
+      if (hasOpposite) {
         // TDB check nex one behind the enemy
+        const moveForward = directions.diff(i.id, id);
+        const moveToId = moveForward(getCoords(i.id), 1);
+        const nextCell = document.getElementById(moveToId);
+        nextCell && i.classList.add('possible-attack');
+        return nextCell;
       }
       return i;
     });
@@ -78,7 +112,6 @@ const calculatePossibleSteps = (id) => {
     value: () => highlightPossibleSteps(res),
   });
 
-  console.log(Object.getOwnPropertyDescriptors(res));
   return res;
 };
 
